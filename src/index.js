300fdd92f85e7395687a20cc21773286b1e7132d
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-08-28 17:54:36 +0200

    added babel

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {graphqlExpress, graphiqlExpress} = require('apollo-server-express')
-const schema = require('./schema')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
+const schema = require('./schema');
 
-// 1 Conect mongodb
-const connectMongo = require('./mongo-connector')
+// 1
+const connectMongo = require('./mongo-connector');
 
 // 2
-const start = () {
-  const mongo = await connectMongo()
-
-  var app = express()
+const start = async () => {
+  // 3
+  const mongo = await connectMongo();
+  var app = express();
   app.use('/graphql', bodyParser.json(), graphqlExpress({
-    context: {mongo},
+    context: {mongo}, // 4
     schema
-  }))
-
+  }));
   app.use('/graphiql', graphiqlExpress({
-    endpointURL: 'graphql'
-  }))
+    endpointURL: '/graphql',
+  }));
 
-  const PORT = 3000
+  const PORT = 3000;
   app.listen(PORT, () => {
     console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-  })
-}
+  });
+};
 
-start()
+// 5
+start();
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,29 +1,18 @@
-const links = [
-  {
-    id: 1,
-    url: 'http://graphql.org/',
-    description: 'The best quey language'
-  },
-  {
-    id: 2,
-    url: 'http://dev.apollodata.com',
-    description: 'Awesome Graphql Client'
-  }
-]
-
 module.exports = {
   Query: {
-    allLinks: async (root, data, {mongo: {Links}}) => {
-      return await Links.find({}).toArray()
+    allLinks: async (root, data, {mongo: {Links}}) => { // 1
+      return await Links.find({}).toArray(); // 2
     },
   },
+
   Mutation: {
     createLink: async (root, data, {mongo: {Links}}) => {
-      const response = await Links.insert(data)
+      const response = await Links.insert(data); // 3
+      return Object.assign({id: response.insertedIds[0]}, data); // 4
     },
   },
 
   Link: {
-    id: root => root._id || root.id,
-  }
-}
+    id: root => root._id || root.id, // 5
+  },
+};
